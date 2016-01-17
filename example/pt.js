351f13d8280739fda24e7d68645363e4a041a910
James Halliday <mail@substack.net>
2016-01-16 21:30:56 -0800

    fixed for the middle term

--- example/pt.js
+++ example/pt.js
@@ -1,14 +1,10 @@
 var intersect = require('../')
 var ecef = require('geodetic-to-ecef')
-var sub = require('gl-vec3/subtract')
-var normalize = require('gl-vec3/normalize')
 
 var pt = ecef(64.50, -121.32, 5000)
-var ground = ecef(64.38, -121.51, 0)
-var ray = sub([], ground, pt)
+var ray = [-0.1,-0.5,-1]
 var ipt = intersect([], pt, ray)
 if (!ipt) return console.log('no intersection')
 
 console.log('point:        [', pt.join(', '), ']')
-console.log('ray:          [', ray.join(', '), ']')
 console.log('intersection: [', ipt.join(', '), ']')
--- index.js
+++ index.js
@@ -8,21 +8,25 @@ module.exports = function (out, p, r) {
   // x(t) = (x+t*u), y(t) = (y+t*v), z(t) = (z+t*w)
   // 0 = pow(x(t)/a,2) + pow(y(t)/a,2) + pow(z(t)/b,2) - 1
   //   = pow((x+t*u)/a,2) + pow((y+t*v)/a,2) + pow((z+t*v)/b,2) - 1
-  //   = (x*x+x*t*u+t*t*u*u)/(a*a)
-  //     + (y*y+y*t*v+t*t*v*v)/(a*a)
-  //     + (z*z+z*t*v+t*t*v*v)/(b*b) - 1
-  //   = x*x/(a*a) + x*t*u/(a*a) + t*t*u*u/(a*a)
-  //     + y*y/(a*a) + y*t*v/(a*a) + t*t*v*v/(a*a)
-  //     + z*z/(b*b) + z*t*w/(b*b) + t*t*w*w/(b*b) - 1
+  //   = (x*x+2*x*t*u+t*t*u*u)/(a*a)
+  //     + (y*y+2*y*t*v+t*t*v*v)/(a*a)
+  //     + (z*z+2*z*t*v+t*t*v*v)/(b*b) - 1
+  //   = x*x/(a*a) + 2*x*t*u/(a*a) + t*t*u*u/(a*a)
+  //     + y*y/(a*a) + 2*y*t*v/(a*a) + t*t*v*v/(a*a)
+  //     + z*z/(b*b) + 2*z*t*w/(b*b) + t*t*w*w/(b*b) - 1
   //   = t*t*((u*u+v*v)/(a*a) + w*w/(b*b))
-  //     + t*((x*u+y*v)/(a*a)+z*w/(b*b))
+  //     + t*2*((x*u+y*v)/(a*a)+z*w/(b*b))
   //     + (x*x+y*y)/(a*a) + z*z/(b*b) - 1
 
-  var qa = (u*u+v*v)/(a*a) + w*w/(b*b)
-  var qb = (x*u+y*v)/(a*a)+z*w/(b*b)
-  var qc = (x*x+y*y)/(a*a) + z*z/(b*b) - 1
-  var t0 = (-qb + Math.sqrt(qb*qb-4*qa*qc)) / (2*qa)
-  var t1 = (-qb - Math.sqrt(qb*qb-4*qa*qc)) / (2*qa)
+  var a2 = a*a, b2 = b*b
+  var qa = (u*u+v*v)/a2 + w*w/b2
+  var qb = 2*((x*u+y*v)/a2 + z*w/b2)
+  var qc = (x*x+y*y)/a2 + z*z/b2 - 1
+  var qsqi = qb*qb-4*qa*qc
+  if (qsqi < 0) return null
+  var qsq = Math.sqrt(qsqi)
+  var t0 = (-qb + qsq) / (2*qa)
+  var t1 = (-qb - qsq) / (2*qa)
   var t
   if (t0 < 0 && t1 < 0) return null // no solution
   else if (t0 > 0 && t1 > 0) t = Math.min(t0, t1)
